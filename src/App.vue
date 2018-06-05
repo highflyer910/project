ef2cbc0dd41dd79482eee273f2e5bc360841e55f
Thea <tmush2014@agruni.edu.ge>
2018-06-05 16:41:25 +0400

    update

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <div class="container"> 
+      <div class="inner">
+        <nav>
+          <router-link to="/" id="logo">Thea Mushambadze</router-link>
+          <ul>
+            <li>
+              <router-link to="/">Home</router-link>
+            </li>
+            <li>
+              <router-link to="/about">About</router-link>
+            </li>
+          </ul>
+        </nav>
+        <transition name="fade" mode="out-in">
+          <router-view/>
+        </transition>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" src="./global.scss">
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="home">
+    <div class="home main-content">
+  	<div class="left-content">
+  		<h1>Front End Developer</h1>
+
+  		<p class="subtext">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+  		tempor incididunt.</p>
+  		<p class="subtext">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+  		tempor incididunt.</p>
+  	</div>
+
+  	<div class="right-content img1">
+  		<button><router-link to="/portfolio/one">Browse Work</router-link></button>
+  	</div>
+  </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style scoped>
+	.img1{
+		background: url('../assets/home.jpg');
+	}
+</style>
