91a039b9deedb4a641ca5385398f54bbf059c0d1
Thea <tmush2014@agruni.edu.ge>
2018-06-18 16:12:34 +0400

    update

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -28,12 +28,7 @@
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
